83caaff8a911410133ae49e2887e3b0dace7b9af
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-17 22:44:29 +0900

    display blog contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import styles from "../components/layout.module.css";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+export default function Home({ allPostsData }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +24,21 @@ export default function Home() {
         <h1 className={`${styles.title} ${utilStyles.alignCenter}`}>
           Welcome to <a href="https://nextjs.org">Kenzo Blog</a>
         </h1>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
       </main>
 
       <footer>
