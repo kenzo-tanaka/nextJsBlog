60c899c37b06f8515fd8eccec901ab7b374626a1
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-18 20:42:34 +0900

    fix [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,10 @@
 import Layout from "../../components/layout";
+import { getAllPostIds } from "../../lib/posts";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export async function Post() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
