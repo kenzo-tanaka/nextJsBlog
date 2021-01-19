80c7be4cb0e44a35d9b3eae630cdb532e4059ee6
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-19 21:14:48 +0900

    feat: シンタックスハイライトを仮のページで実装

---
+++ pages/hoge.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+
+const renderers = {
+  code: ({ language, value }) => {
+    return <SyntaxHighlighter language={language} children={value} />;
+  },
+};
+
+const markdown = `Here is some JavaScript code:
+
+~~~js
+console.log('It works!')
+~~~
+`;
+
+export default function Hoge() {
+  return (
+    <>
+      <ReactMarkdown renderers={renderers} children={markdown} />
+    </>
+  );
+}
