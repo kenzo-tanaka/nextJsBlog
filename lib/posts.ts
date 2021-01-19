ce8ff4e981efcd20b7b5a0f8cef8e1f774be5040
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-19 18:34:34 +0900

    render markdown post on show page

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,6 +2,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import remark from "remark";
+import html from "remark-html";
 
 const postDir = path.join(process.cwd(), "posts");
 
@@ -41,14 +43,20 @@ export const getAllPostIds = () => {
   });
 };
 
-export function getPostData(id: string) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postDir, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const matterResult = matter(fileContents);
 
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
   // Combine the data with the id
   return {
     id,
+    contentHtml,
     ...matterResult.data,
   };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -5,7 +5,7 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
