b562f98c2871e1e39fd84db55a43ae5f16b15b9c
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-19 21:29:51 +0900

    fix: ReactMarkdownを使ってmarkdown -> htmlに変換

--- pages/hoge.tsx
+++ pages/hoge.tsx
@@ -1,3 +1,5 @@
+// TODO: ファイルを削除
+
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,6 +3,7 @@ import Layout from "../../components/layout";
 import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
+import ReactMarkdown from "react-markdown";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -32,9 +33,8 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-
-        {/* TODO: markdown -> html */}
-        <div>{postData.content}</div>
+        {/* TODO: シンタックスハイライト */}
+        <ReactMarkdown children={postData.content} />
       </article>
     </Layout>
   );
