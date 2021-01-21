cfca64920c2b0f4a20b4913b2517b4311a5c31df
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-21 21:39:42 +0900

    fix: 不要なimportを削除

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,8 +2,6 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import remark from "remark";
-import html from "remark-html";
 
 const postDir = path.join(process.cwd(), "posts");
 
