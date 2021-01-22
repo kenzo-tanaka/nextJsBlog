bf3b7e894e1fb2a1c3e0fd0d5bb19bf7ac947d2c
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-22 18:43:48 +0900

    add about component

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function About() {
+  return (
+    <Layout>
+      {/* TODO: ハードコーディングしたくないので markdown を読むようにする */}
+      <p>About me</p>
+    </Layout>
+  );
+}
