1163864daf4bd424972117e62b52c5c27d85b170
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-22 21:57:46 +0900

    fix: render about page with md file

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,24 @@
+import Head from "next/head";
 import Layout from "../components/layout";
+import { getPageData } from "../lib/page";
+import ReactMarkdown from "react-markdown";
 
-export default function About() {
+export async function getStaticProps() {
+  const pageData = await getPageData("about");
+  return {
+    props: {
+      pageData,
+    },
+  };
+}
+
+export default function About({ pageData }) {
   return (
     <Layout>
-      {/* TODO: ハードコーディングしたくないので markdown を読むようにする */}
-      <p>About me</p>
+      <Head>
+        <title>{pageData.title}</title>
+      </Head>
+      <ReactMarkdown children={pageData.content} />
     </Layout>
   );
 }
