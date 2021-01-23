38429b727621508b62efadcd0c26a5d1a3aee4f5
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-23 21:05:35 +0900

    add site.config.ts

---
+++ site.config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  siteMeta: {
+    title: "Kenzo Blog",
+    description: "Tech Blog by Kenzo Tanaka",
+  },
+  siteRoot:
+    process.env.NODE_ENV === "production"
+      ? "https://kenzoblog.vercel.app/"
+      : "http://localhost:3000",
+};
