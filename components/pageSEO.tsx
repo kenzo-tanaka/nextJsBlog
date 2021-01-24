556e336abb5cb60e3c58fea4dcbfc4a02d8fca50
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-24 11:17:11 +0900

    add ogp image fix pageSEO component

--- components/pageSEO.tsx
+++ components/pageSEO.tsx
@@ -3,18 +3,26 @@ import { config } from "../site.config";
 
 type Props = {
   title: string;
+  slug?: string;
   description?: string;
   ogImageUrl?: string;
 };
 
 export const pageSEO: React.FC<Props> = (props) => {
-  const { title, description, ogImageUrl } = props;
-
+  const { title, slug, description, ogImageUrl } = props;
+  const pageUrl = `${config.siteRoot}/${slug || ""}`;
   return (
     <Head>
       <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content={pageUrl} />
       <meta name="description" content={description} />
-      <meta property="og:image" content={ogImageUrl} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:site" content={config.siteMeta.title} />
+      <meta
+        property="og:image"
+        content={ogImageUrl || `${config.siteRoot}/images/ogp.jpg`}
+      />
     </Head>
   );
 };
