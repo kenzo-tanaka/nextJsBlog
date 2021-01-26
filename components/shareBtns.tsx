0efced9deff494b2aa601fd4ae488e6d991a672a
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-26 15:44:46 +0900

    fix: シェアボタン周りを styled-components をやめて CSS Modules でスタイリング

--- components/shareBtns.tsx
+++ components/shareBtns.tsx
@@ -1,57 +1,38 @@
-import styled from "styled-components";
 import { config } from "../site.config";
-
-const BtnsWrapper = styled.div`
-  margin-top: 1.8em;
-  text-align: center;
-`;
-
-const SnsBtn = styled.a`
-  display: inline-block;
-  text-decoration: none;
-  width: 50px;
-  margin: 2px;
-  height: 50px;
-  line-height: 50px;
-  font-size: 23px;
-  color: white;
-  border-radius: 50px;
-  text-align: center;
-  overflow: hidden;
-  font-weight: bold;
-  transition: 0.3s;
-  background: #000;
-`;
+import utilStyles from "../styles/utils.module.css";
 
 export default function ShareBtns({ slug, title }) {
   const pageUrl = `${config.siteRoot}/posts/${slug}`;
   const postTitle = encodeURIComponent(`${title} | Kenzo Blog`);
   return (
-    <BtnsWrapper>
-      <SnsBtn
+    <div className={utilStyles.btnsWrapper}>
+      <a
         href={`https://twitter.com/share?url=${pageUrl}&text=${postTitle}&via=kenzoooooB`}
         rel="nofollow"
+        className={utilStyles.snsBtn}
         style={{ padding: "4px" }}
       >
         <img
           src="/images/twitter-white.svg"
           alt="Twitter"
-          style={{ maxWidth: "44px", margin: "4px", display: "inline-block" }}
+          style={{ maxWidth: "44px", margin: "4px", display: "inline" }}
         />
-      </SnsBtn>
-      <SnsBtn
+      </a>
+      <a
         href={`https://www.facebook.com/share.php?u=${pageUrl}`}
         rel="nofollow"
+        className={utilStyles.snsBtn}
         style={{ margin: "0 0.5em 0 0.5em" }}
       >
         F
-      </SnsBtn>
-      <SnsBtn
+      </a>
+      <a
         href={`http://b.hatena.ne.jp/add?mode=confirm&url=${pageUrl}`}
+        className={utilStyles.snsBtn}
         rel="nofollow"
       >
         B!
-      </SnsBtn>
-    </BtnsWrapper>
+      </a>
+    </div>
   );
 }
