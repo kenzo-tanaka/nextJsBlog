29a36041363d4e7ee3527e5aab544c5d8be5463c
kenzo <kenzou.kenzou104809@gmail.com>
2021-01-30 09:49:21 +0900

    fix date.tsx

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,12 @@
 import { parseISO, format, parse } from "date-fns";
 
-export default function Date({ dateString }) {
+type Props = {
+  dateString: string;
+};
+
+const Date: React.FC<Props> = ({ dateString }) => {
   const date = parseISO(dateString);
   return <time dateTime={dateString}>{format(date, "yyyy/LL/dd")}</time>;
-}
+};
+
+export default Date;
