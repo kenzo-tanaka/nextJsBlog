7e7bbfffedb07154e7a74f5eeb512bd1f2708989
kenzo <kenzou.kenzou104809@gmail.com>
2021-02-02 09:38:11 +0900

    add lib/categories.ts

---
+++ lib/categories.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { getMatterResult } from "./matter";
+import { config } from "../site.config";
+
+const postDir = config.postDir;
+
+export async function getCategoryPosts(name: string) {
+  const fileNames = fs.readdirSync(postDir);
+  const categoryPosts = fileNames
+    .filter((fileName) => {
+      const matterResult = getMatterResult(path.join(postDir, fileName));
+      matterResult.data.category === name;
+    })
+    .map((fileName) => {
+      const matterResult = getMatterResult(path.join(postDir, fileName));
+      return {
+        slug: fileName.replace(/\.md$/, ""),
+        content: matterResult.content,
+        date: matterResult.data.date,
+        category: matterResult.data.category,
+      };
+    });
+
+  return categoryPosts.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+export const getCategoryNames = () => {
+  const fileNames = fs.readdirSync(postDir);
+  return fileNames.map((fileName) => {
+    const matterResult = getMatterResult(path.join(postDir, fileName));
+    return {
+      params: {
+        name: matterResult.data.category,
+      },
+    };
+  });
+};
