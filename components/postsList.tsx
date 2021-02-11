197aa4525d3e211fc1355caa6f994ce70b191ca4
kenzo <kenzou.kenzou104809@gmail.com>
2021-02-11 13:47:06 +0900

    fix post list styles

--- components/postsList.tsx
+++ components/postsList.tsx
@@ -15,21 +15,21 @@ const PostsList: NextPage<Props> = ({ posts, heading }) => {
       <h2 className={utilStyles.headingLg}>{heading}</h2>
       <ul className={utilStyles.list}>
         {posts.map(({ slug, date, title, category }) => (
-          <li className={utilStyles.listItem} key={slug}>
+          <li className="mt-3" key={slug}>
             <Link href={`/posts/${slug}`}>
-              <a className={`${utilStyles.titleText}`}>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-            <Link href={`/categories/${category}`}>
-              <a
-                className={`${utilStyles.categoryLabel} ${utilStyles.linkUnderline}`}
-              >
-                #{category}
+              <a className="text-lg font-semibold visited:text-gray-600">
+                {title}
               </a>
             </Link>
+            <br />
+            <small className="text-sm font-normal text-gray-400">
+              <Date dateString={date} />
+            </small>
+            {/* <Link href={`/categories/${category}`}>
+              <a className="text-sm text-gray-400 ml-1 font-normal underline">
+                #{category}
+              </a>
+            </Link> */}
           </li>
         ))}
       </ul>
