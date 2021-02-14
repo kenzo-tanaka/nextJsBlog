0c0609fdfe87a5d30c2d6b48e620980369018ed1
kenzo <kenzou.kenzou104809@gmail.com>
2021-02-14 20:57:36 +0900

    fix: react-markdown settings in about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Layout from "../components/layout";
 import { getPageData } from "../lib/page";
+import { isURL } from "./posts/[slug]";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 
@@ -18,17 +19,35 @@ type Props = {
   pageData: {
     title: string;
     content: string;
+    slug: string;
   };
 };
 
 const About: NextPage<Props> = ({ pageData }) => {
+  const { slug, title, content } = pageData;
+
+  const Img = ({ alt, src }: { alt: string; src: string }) => {
+    return (
+      <picture>
+        <img
+          src={isURL(src) ? src : require(`../contents/pages/${slug}/${src}`)}
+          alt={alt}
+        />
+      </picture>
+    );
+  };
+
   return (
     <Layout>
       <Head>
-        <title>{pageData.title}</title>
+        <title>{title}</title>
       </Head>
       <div className="markdown-body p-7">
-        <ReactMarkdown plugins={[gfm]} children={pageData.content} />
+        <ReactMarkdown
+          renderers={{ image: Img }}
+          plugins={[gfm]}
+          children={content}
+        />
       </div>
     </Layout>
   );
