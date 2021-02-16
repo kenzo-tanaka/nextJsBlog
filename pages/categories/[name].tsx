081f9bee58773ecac427f12a50a98e545252cb1c
kenzo <kenzou.kenzou104809@gmail.com>
2021-02-16 09:39:42 +0900

    fix category page

--- pages/categories/[name].tsx
+++ pages/categories/[name].tsx
@@ -3,6 +3,7 @@ import Layout from "../../components/layout";
 import PostsList from "../../components/postsList";
 import Profile from "../../components/profile";
 import CategoryMenu from "../../components/categoryMenu";
+import MobileCategoryMenu from "../../components/MobileCategoryMenu";
 import { PageSEO } from "../../components/pageSEO";
 import { getCategoryPosts, getCategoryNames } from "../../lib/categories";
 import { PostData } from "@types";
@@ -34,17 +35,10 @@ const Category: NextPage<Props> = ({ posts, categoryName }) => {
   return (
     <Layout>
       <PageSEO title={categoryName} slug={`categories/${categoryName}`} />
-      <div className="shadow-md p-5 divide-solid">
-        {/* Desktop */}
-        <div className="hidden md:block">
-          <Profile />
-          <hr className="my-5" />
-        </div>
-        {/* Mobile */}
-        <div className="block md:hidden">
-          <p className="text-gray-500">#{categoryName}</p>
-          <hr className="my-5" />
-        </div>
+      <div className="shadow-md p-5">
+        <Profile />
+        <MobileCategoryMenu />
+        <hr className="my-5 hidden sm:block" />
         <div className="grid grid-cols-6 gap-4">
           <div className="hidden md:block">
             <CategoryMenu />
