0f7edc6a05fa04e8222354ae981a62e694c70c42
kenzo <kenzou.kenzou104809@gmail.com>
2021-02-24 11:48:15 +0900

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const { is } = require("date-fns/locale");
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([[optimizedImages, {}]], {
-  webpack: function (config) {
+  webpack: (config, { isSever }) => {
+    // Module not found: Can't resolve 'fs' の対応
+    // ref: https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
+    if (!isSever) {
+      config.node = {
+        fs: "empty",
+      };
+    }
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
