77d44cb793ac173274b95fe9a54d92023b8e3913
kenzo <kenzou.kenzou104809@gmail.com>
2021-04-10 16:35:49 +0900

    max-widthを調整

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Layout: React.FC<Props> = ({ children, home = false }) => {
   return (
-    <div className="max-w-3xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div className="max-w-4xl mx-auto px-2 sm:px-6 lg:px-8">
       <main>
         <div className="shadow-md bg-white">{children}</div>
       </main>
--- components/navBar.tsx
+++ components/navBar.tsx
@@ -5,7 +5,7 @@ import { config } from "../site.config";
 const NavBar: React.FC = () => {
   return (
     <nav className="bg-white border-b border-gray-200">
-      <div className="max-w-3xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
