19809ed3db4fe42bd9d6c4db17bb93b68224cb52
kenzo <kenzou.kenzou104809@gmail.com>
2021-04-17 17:51:43 +0900

    fix: mocks/下に作成したファイルなどが無くてもテストを実行できるので削除した。その他jest.config.jsの不要な記述を削除 https://github.com/vercel/next.js/tree/canary/examples/with-jest

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,14 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  // roots: ["<rootDir>/__tests__"],
-  setupFilesAfterEnv: ["<rootDir>/components/__tests__/setupTests.ts"],
-  testPathIgnorePatterns: [
-    "<rootDir>/components/__tests__/setupTests.ts",
-    "<rootDir>/components/__tests__/tsconfig.jest.json",
-    "<rootDir>/components/__tests__/mocks/",
-  ],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   // https://github.com/zeit/next.js/issues/8663#issue-490553899
   globals: {
-    // we must specify a custom tsconfig for tests because we need the typescript transform
-    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
-    // can see this setting in tsconfig.jest.json -> "jsx": "react"
     "ts-jest": {
       tsconfig: "<rootDir>/tsconfig.jest.json",
     },
-  },
-  collectCoverage: false,
-  collectCoverageFrom: ["src/**/*"],
-  coverageDirectory: "./coverage/",
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/__tests__/mocks/styleMock.js",
-    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__tests__/mocks/fileMock.js",
   },
 };
