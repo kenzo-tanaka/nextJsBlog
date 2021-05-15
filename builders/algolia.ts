b4fbe72d9b93029f046ecf1ca510d1ee327fd547
kenzo <kenzou.kenzou104809@gmail.com>
2021-05-15 15:14:56 +0900

    builders/algolia: remove todos

--- builders/algolia.ts
+++ builders/algolia.ts
@@ -9,13 +9,11 @@ const getArticleMeta = () => {
   //   console.log("正常に書き込みが完了しました");
   // });
   // TODO: data/下のalgoliaファイルの最新ファイルのものを取得
-  // TODO: fileをReedする
   fs.readFile("./data/20210509-algolia.json", "utf8", (err, postsString) => {
     const postsArray = JSON.parse(postsString);
     console.log(postsArray);
   });
 
-  // TODO: Readした内容をオブジェクトの配列として変数に入れる
   // TODO: 既存の記事一覧との差分の配列を作成する
   // TODO: 20210515のような日付つきファイルを作成して保存
   // TODO: できればそのままAlgolia APIを使ってFileをアップロードする
