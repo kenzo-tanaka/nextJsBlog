d2b4798b4011201100a90b9c8d64eac91c1f1e9a
kenzo <kenzou.kenzou104809@gmail.com>
2021-05-15 15:01:15 +0900

    builders/algolia: read json and parse

--- builders/algolia.ts
+++ builders/algolia.ts
@@ -10,6 +10,11 @@ const getArticleMeta = () => {
   // });
   // TODO: data/下のalgoliaファイルの最新ファイルのものを取得
   // TODO: fileをReedする
+  fs.readFile("./data/20210509-algolia.json", "utf8", (err, postsString) => {
+    const postsArray = JSON.parse(postsString);
+    console.log(postsArray);
+  });
+
   // TODO: Readした内容をオブジェクトの配列として変数に入れる
   // TODO: 既存の記事一覧との差分の配列を作成する
   // TODO: 20210515のような日付つきファイルを作成して保存
