568e5c4f4d4edc8c1fa976dec4693dc3c54a8ff0
kenzo <kenzou.kenzou104809@gmail.com>
2021-05-15 14:51:51 +0900

    builders/algolia: add todos

--- builders/algolia.ts
+++ builders/algolia.ts
@@ -2,13 +2,18 @@ import fs from "fs";
 import { getSortedPostsData } from "../lib/posts";
 
 const getArticleMeta = () => {
-  const posts = getSortedPostsData();
-  const data = JSON.stringify(posts);
-
-  fs.writeFile("algolia.json", data, (err) => {
-    if (err) throw err;
-    console.log("正常に書き込みが完了しました");
-  });
+  // const posts = getSortedPostsData();
+  // const data = JSON.stringify(posts);
+  // fs.writeFile("algolia.json", data, (err) => {
+  //   if (err) throw err;
+  //   console.log("正常に書き込みが完了しました");
+  // });
+  // TODO: data/下のalgoliaファイルの最新ファイルのものを取得
+  // TODO: fileをReedする
+  // TODO: Readした内容をオブジェクトの配列として変数に入れる
+  // TODO: 既存の記事一覧との差分の配列を作成する
+  // TODO: 20210515のような日付つきファイルを作成して保存
+  // TODO: できればそのままAlgolia APIを使ってFileをアップロードする
 };
 
 getArticleMeta();
