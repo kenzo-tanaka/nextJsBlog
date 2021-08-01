d85cdb6acf461b3fcb3368e006a5a25c1e24107e
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-01 17:41:17 +0900

    feat: /qiita用のページを作成

--- pages/qiita.tsx
+++ pages/qiita.tsx
@@ -1,19 +1,18 @@
 import { NextPage } from "next";
 import Layout from "../components/layout";
 import { PageSEO } from "../components/pageSEO";
-import { ExternalPostData } from "@types";
-import ZennPosts from "../contents/zenn/articles.json"
+import QiitaPosts from "../contents/qiita/articles.json"
 import ExternalPostList from "../components/externalPostList";
 
-const Zenn: NextPage = () => {
+const Qiita: NextPage = () => {
   return (
     <Layout home>
-      <PageSEO title='Zennへの投稿一覧' />
+      <PageSEO title='Qiitaへの投稿一覧' />
       <div className="p-5">
-        <ExternalPostList posts={ZennPosts} />
+        <ExternalPostList posts={QiitaPosts} />
       </div>
     </Layout>
   );
 };
 
-export default Zenn;
+export default Qiita;
