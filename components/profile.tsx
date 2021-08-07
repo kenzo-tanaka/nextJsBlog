671d1267f724e3e927d2aab3efc606acd214c663
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-07 12:13:20 +0900

    fix: Profileの画像を next/image ではなく通常のimgタグで書き換え next/imageによる最適化によって rounded-full の整形が外されている可能性を検証するため

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 
 const Profile: React.FC = () => {
   return (
     <div className="flex flex-col	items-center">
-      <picture>
-        <source srcSet="/images/profile.jpg" />
-        <Image
-          src="/images/profile.jpg"
-          alt="Picture of the author"
-          className="rounded-full"
-          width={100}
-          height={100}
-        />
-      </picture>
+      <img
+        src="/images/profile.jpg"
+        alt="Picture of the author"
+        className="rounded-full"
+        width={100}
+        height={100}
+      />
       <p className="text-gray-500 mt-4">
         Personal tech blog /{" "}
         <Link href="/about">
