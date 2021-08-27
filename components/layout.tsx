b587512dc54ab57b8cfd09929b6e9f702b047415
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-27 19:24:02 +0900

    fix: 画面最大幅を修正

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Layout: React.FC<Props> = ({ children, home = false }) => {
   return (
-    <div className="max-w-4xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div className="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8">
       <Search />
       <main className="mt-3">
         <div className="shadow-md bg-white">{children}</div>
--- components/navBar.tsx
+++ components/navBar.tsx
@@ -6,7 +6,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const NavBar: React.FC = () => {
   return (
     <nav className="bg-white border-b border-gray-200">
-      <div className="max-w-4xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div className="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
