29784d47f13dabc4a0e7a6fb0eef26e2b6fbce47
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-28 17:07:58 +0900

    fix: Change search box styles with tailwind.

--- components/search.tsx
+++ components/search.tsx
@@ -35,11 +35,20 @@ const SearchResult = () => {
 };
 
 const SearchBox = ({ currentRefinement, refine }: any) => (
-  <input
-    type="text"
-    value={currentRefinement}
-    onChange={event => refine(event.currentTarget.value)}
-  />
+  <div className="bg-white shadow p-2 flex rounded-md">
+    <span className="w-auto flex justify-end items-center text-gray-500 p-2">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
+    </span>
+    <input
+      className='w-full rounded p-2 outline-none'
+      type="text"
+      placeholder='記事を検索'
+      value={currentRefinement}
+      onChange={event => refine(event.currentTarget.value)}
+    />
+  </div>
 );
 
 const CustomSearchBox = connectSearchBox(SearchBox);
