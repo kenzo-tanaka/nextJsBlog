127c4855ff1711f027789a39b6a28bc39b31bf52
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-28 19:59:28 +0900

    fix: Change pagination color

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -18,7 +18,7 @@ const Pagination: React.FC<Props> = ({ totalCount, current }) => {
       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
         <div className="mx-1 inline-block" key={index}>
           <Link href={`/posts/page/${number}`}>
-            <a className={`${current === number ? 'bg-indigo-50 border-indigo-500 text-indigo-600' : ''} rounded-sm z-10 relative inline-flex items-center px-4 py-2 px-3 border border-gray-300 text-sm font-medium`}>
+            <a className={`${current === number ? 'bg-gray-100 border-gray-500' : ''} rounded-sm z-10 relative inline-flex items-center px-4 py-2 px-3 border border-gray-300 text-sm font-medium`}>
               {number}
             </a>
           </Link>
