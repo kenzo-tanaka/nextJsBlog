667019048537c47b31ed7dfe47fb1d7c9e189d67
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-28 11:20:37 +0900

    feat: Fix categoryManu logic for pagination.

--- components/categoryMenu.tsx
+++ components/categoryMenu.tsx
@@ -6,16 +6,16 @@ import { config } from "../site.config";
 const CategoryMenu: React.FC = () => {
   const router = useRouter();
   const categoryList = config.categoryList;
+
   return (
     <div>
       {categoryList.map(({ slug, name }) => (
         <Link href={slug} key={slug}>
           <a
-            className={`p-2 font-medium text-gray-800 w-full inline-block hover:bg-gray-100 hover:no-underline ${
-              router.asPath === slug
-                ? "border-l-4 border-gray-500 bg-gray-100"
-                : ""
-            }`}
+            className={`p-2 font-medium text-gray-800 w-full inline-block hover:bg-gray-100 hover:no-underline ${router.asPath === slug || (slug === '/' && router.asPath.match(/posts\/page/))
+              ? "border-l-4 border-gray-500 bg-gray-100"
+              : ""
+              }`}
           >
             {name}
           </a>
