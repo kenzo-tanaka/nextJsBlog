66581bf9c6b7c2a90c525cbec626348edce8701a
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-28 12:39:48 +0900

    fix: Split to func for checking current path.

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -7,8 +7,17 @@ type Props = {
   totalCount: number;
 }
 
-const Pagination: React.FC<Props> = ({ totalCount }) => {
+const isCurrent = (num: number) => {
   const router = useRouter();
+
+  if (router.asPath === '/' || router.asPath === null) {
+    return false
+  } else if (parseInt(router.asPath.match(/\d+/)[0]) === num) {
+    return true
+  }
+}
+
+const Pagination: React.FC<Props> = ({ totalCount }) => {
   const PER_PAGE = config.postsNumPerPage;
 
   const range = (start: number, end: number) =>
@@ -19,7 +28,7 @@ const Pagination: React.FC<Props> = ({ totalCount }) => {
       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
         <div className="mx-1 inline-block">
           <Link href={`/posts/page/${number}`}>
-            <a className={`rounded-sm z-10 relative inline-flex items-center px-4 py-2 px-3 border border-gray-300 text-sm font-medium ${(router.asPath !== '/' && parseInt(router.asPath.match(/\d+/)[0]) === number) ? 'bg-indigo-50 border-indigo-500 text-indigo-600' : ''}`} key={index}>
+            <a className={`${isCurrent(number) ? 'bg-indigo-50 border-indigo-500 text-indigo-600' : ''} rounded-sm z-10 relative inline-flex items-center px-4 py-2 px-3 border border-gray-300 text-sm font-medium`} key={index}>
               {number}
             </a>
           </Link>
