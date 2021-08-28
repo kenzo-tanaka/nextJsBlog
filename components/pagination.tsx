7bb9d4aba916dbc1e0cf0a3fb6d3f194306e0b50
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-28 10:17:30 +0900

    chore: Fix pagenation styles

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
+import { config } from '../site.config'
 
 type Props = {
   totalCount: number;
 }
 
 const Pagination: React.FC<Props> = ({ totalCount }) => {
-  const PER_PAGE = 10;
+  const PER_PAGE = config.postsNumPerPage;
 
   const range = (start: number, end: number) =>
     [...Array(end - start + 1)].map((_, i) => start + i)
 
   return (
-    <ul>
+    <nav className='relative z-0 inline-flex rounded-md shadow-sm'>
       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
-        <li key={index}>
-          <Link href={`/blog/page/${number}`}>
-            <a>{number}</a>
-          </Link>
-        </li>
+        <Link href={`/posts/page/${number}`}>
+          <a className='rounded-sm z-10 relative inline-flex items-center px-4 py-2 px-3 border text-sm font-medium' key={index}>
+            {number}
+          </a>
+        </Link>
       ))}
-    </ul>
+    </nav>
   );
 };
 
