8fcd3f057eb84f0fd0377cee47c5d8d377e0cfc4
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-29 16:22:52 +0900

    fix: Resolve `configuration[0].node has an unknown property 'fs'.` error

--- next.config.js
+++ next.config.js
@@ -2,13 +2,12 @@ const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 module.exports = withPlugins([[optimizedImages, {}]], {
-  webpack: (config, { isSever }) => {
+  webpack: (config, { isServer }) => {
     // Module not found: Can't resolve 'fs' の対応
     // ref: https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
-    if (!isSever) {
-      config.node = {
-        fs: "empty",
-      };
+    // ref: https://github.com/vercel/next.js/issues/7755#issuecomment-812805708
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
     }
     config.module.rules.push({
       test: /\.md$/,
