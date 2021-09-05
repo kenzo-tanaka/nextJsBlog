482341f74d4f23b8164ee21d247aacb628ddf8b8
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-09-05 17:11:38 +0900

    fix: Separate a function to request to Qiita api.

--- builders/qiita.ts
+++ builders/qiita.ts
@@ -1,4 +1,5 @@
 const request = require('request');
+const axios = require('axios').default;
 const _ = require('lodash');
 import fs from "fs-extra";
 import { ExternalPostData } from "@types";
@@ -7,21 +8,31 @@ import { config } from "../site.config";
 const articleJsonPath: string = './contents/qiita/articles.json'
 const pastPosts: ExternalPostData[] = fs.readJSONSync(articleJsonPath);
 
-request(`https://qiita.com/api/v2/users/${config.qiitaId}/items`, function (error: Object, response: Object, body: string) {
-  const currentPosts: ExternalPostData[] = JSON.parse(body).map((element: { title: string, created_at: string, url: string }) => {
-    return (
-      {
+const getPosts = async (url: string) => {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+const main = async () => {
+  try {
+    const posts = await getPosts(`https://qiita.com/api/v2/users/${config.qiitaId}/items`)
+    const currentPosts: ExternalPostData[] = posts.map((element: { title: string, created_at: string, url: string }) => {
+      return {
         'title': element['title'],
         'created_at': element['created_at'],
         'url': element['url'],
       }
-    )
-  });
+    })
 
-  if (_.isEqual(pastPosts, currentPosts)) {
-    console.log('Qiitaの記事は更新がなかったのでファイルを更新しませんでした。');
-  } else {
-    fs.writeJsonSync(articleJsonPath, currentPosts);
-    console.log('Qiitaの新しい記事を反映しました。');
+    if (_.isEqual(pastPosts, currentPosts)) {
+      console.log('Qiitaの記事は更新がなかったのでファイルを更新しませんでした。');
+    } else {
+      fs.writeJsonSync(articleJsonPath, currentPosts);
+      console.log('Qiitaの新しい記事を反映しました。');
+    }
+  } catch (error) {
+    console.log(error);
   }
-})
+}
+
+main();
