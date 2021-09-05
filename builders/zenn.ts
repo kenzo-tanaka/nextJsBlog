039b9ae3db9a96d98d3581dfb64561c0c5ac3d8d
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-09-05 17:25:38 +0900

    fix: Fix whitespace

--- builders/zenn.ts
+++ builders/zenn.ts
@@ -7,29 +7,29 @@ import { config } from "../site.config";
 const articleJsonPath: string = './contents/zenn/articles.json'
 const pastPosts: ExternalPostData[] = fs.readJSONSync(articleJsonPath);
 const getPosts = async (url: string) => {
-	const response = await axios.get(url);
-	return response.data;
+  const response = await axios.get(url);
+  return response.data;
 }
 
 const main = async () => {
-	try {
-		const posts = await getPosts(`https://zenn.dev/api/articles?username=${config.zennId}&order=latest`);
-		const currentPosts: ExternalPostData[] = posts['articles'].map((element: { title: string, created_at: string, slug: string }) => {
-			return {
-				'title': element['title'],
-				'created_at': element['created_at'],
-				'url': 'https://zenn.dev/kenzo/articles/' + element['slug'],
-			}
-		});
-		if (_.isEqual(pastPosts, currentPosts)) {
-			console.log('Zennの記事は更新がなかったのでファイルを更新しませんでした。')
-		} else {
-			fs.writeJsonSync(articleJsonPath, currentPosts);
-			console.log('Zennの新しい記事を反映しました。');
-		};
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const posts = await getPosts(`https://zenn.dev/api/articles?username=${config.zennId}&order=latest`);
+    const currentPosts: ExternalPostData[] = posts['articles'].map((element: { title: string, created_at: string, slug: string }) => {
+      return {
+        'title': element['title'],
+        'created_at': element['created_at'],
+        'url': 'https://zenn.dev/kenzo/articles/' + element['slug'],
+      }
+    });
+    if (_.isEqual(pastPosts, currentPosts)) {
+      console.log('Zennの記事は更新がなかったのでファイルを更新しませんでした。')
+    } else {
+      fs.writeJsonSync(articleJsonPath, currentPosts);
+      console.log('Zennの新しい記事を反映しました。');
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 main();
