5356d604c22e57afb25b0b30f2b826c1286d6345
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-09-05 10:14:10 +0900

    fix: Declare a variable of article json path.

--- builders/qiita.ts
+++ builders/qiita.ts
@@ -3,7 +3,9 @@ import fs from "fs-extra";
 import { ExternalPostData } from "@types";
 import { config } from "../site.config";
 
-request(`https://qiita.com/api/v2/users/${config.qiitaId}/items`, function (_error: any, _response: any, body: string) {
+const articleJsonPath = './contents/qiita/articles.json'
+
+request(`https://qiita.com/api/v2/users/${config.qiitaId}/items`, function (error: Object, response: Object, body: string) {
   const data = JSON.parse(body)
   const articles: ExternalPostData[] = [];
   data.forEach((element: any) => {
@@ -15,11 +17,11 @@ request(`https://qiita.com/api/v2/users/${config.qiitaId}/items`, function (_err
     articles.push(article)
   });
 
-  const pastPosts = fs.readJSONSync('./contents/qiita/articles.json');
+  const pastPosts = fs.readJSONSync(articleJsonPath);
   if (JSON.stringify(pastPosts) === JSON.stringify(articles)) {
     console.log('Qiitaの記事は更新がなかったのでファイルを更新しませんでした。')
     return;
   } else {
-    fs.writeJsonSync("./contents/zenn/articles.json", articles);
+    fs.writeJsonSync(articleJsonPath, articles);
   }
 })
