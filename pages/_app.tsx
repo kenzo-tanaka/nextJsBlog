558ebe695b843ede53d4031642f39735f3d8b97b
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-09-06 21:00:16 +0900

    fix: Change bg color.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Navbar />
-      <div className="bg-gray-50 pt-3 pb-12">
+      <div className="bg-gray-100 pt-3 pb-12">
         <Component {...pageProps} />
       </div>
     </>
