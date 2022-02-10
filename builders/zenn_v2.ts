c198b9d236fd4d2943bb77306f4ae466a7d8c68b
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-10 21:11:51 +0900

    Fix zenn v2

--- builders/zenn_v2.ts
+++ builders/zenn_v2.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { config } from '../site.config'
+import { ExternalPostData } from "@types";
 
 const getPosts = async (url: string) => {
   const response = await axios.get(url)
@@ -8,12 +9,12 @@ const getPosts = async (url: string) => {
 
 export const getZennPosts = async () => {
   const posts = await getPosts(`https://zenn.dev/api/articles?username=${config.zennId}&order=latest`);
-
-  return (
-    [{
-      title: '一次情報を検索しやすくするChrome拡張を作った。',
-      created_at: '2021/10/30',
-      url: 'https://zenn.dev/kenzo/articles/e69f87236b479f'
-    }]
-  )
+  const currentPosts: ExternalPostData[] = posts['articles'].map((element: { title: string, created_at: string, slug: string }) => {
+    return {
+      'title': element['title'],
+      'created_at': element['created_at'],
+      'url': 'https://zenn.dev/kenzo/articles/' + element['slug'],
+    }
+  });
+  return currentPosts
 }
