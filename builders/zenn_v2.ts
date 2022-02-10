f8552f4bb466bb7d5fe07ee31bb6c53809c866fb
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-10 19:21:36 +0900

    Add zenn_v2

--- builders/zenn_v2.ts
+++ builders/zenn_v2.ts
@@ -1,12 +1,9 @@
-import { getZennPosts } from "../zenn_v2"
-
-describe("zenn", () => {
-  test("zenn", () => {
-    const result = getZennPosts()
-    expect(result).toEqual([{
+export const getZennPosts = () => {
+  return (
+    [{
       title: '一次情報を検索しやすくするChrome拡張を作った。',
       created_at: '2021/10/30',
       url: 'https://zenn.dev/kenzo/articles/e69f87236b479f'
-    }])
-  })
-})
+    }]
+  )
+}
